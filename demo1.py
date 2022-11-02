dc6d01bcd40906496bb991aaa986134eeabd324c
vmuu <68872185@qq.com>
2022-11-02 11:33:49 +0800

    update

--- demo1.py
+++ demo1.py
@@ -2,6 +2,7 @@ import math
 import random
 
 print(math.sin(0.5))
-x = random.random()
-y = random.randint(1, 100)
+x = random.random()  # 获得（0,1）内的的随机小数
+y = random.randint(1, 100)  # 获得（1,100）的随机整数
+
 print(x, y, sep="\n")
